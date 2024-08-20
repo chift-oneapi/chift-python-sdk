622ca83ba56b2640d79a31afb9a09dc86130eb2e
Ihor Kedenko <ihor.kedenko@chift.eu>
2024-08-20 23:01:06 +0300

    Added ChiftProcessControlledException.

--- chift/api/exceptions.py
+++ chift/api/exceptions.py
@@ -8,3 +8,14 @@ class ChiftException(Exception):
 
     def __str__(self):
         return f"Chift Exception: {self.error_code} {self.message} {self.detail}"
+
+
+class ChiftProcessControlledException(ChiftException):
+    """Exception for handle controlled errors in process execution"""
+    def __init__(self, message, error_status_code, error_code=None, detail=None):
+        super().__init__(message, error_code, detail)
+        self.error_status_code = error_status_code
+
+    def __str__(self):
+        return (f"Chift Exception: {self.error_code} {self.message} {self.detail}. "
+                f"Error Process status code - {self.error_status_code}")
