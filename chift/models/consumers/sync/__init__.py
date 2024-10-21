cbffeb10b2f6324d41fada8eb47c31cb451d67a4
Florian Gras <florian.gras@chift.eu>
2024-10-21 17:29:12 +0200

    sync: add update mixin for sync configuration modification

--- chift/models/consumers/sync/__init__.py
+++ chift/models/consumers/sync/__init__.py
@@ -1,10 +1,10 @@
 from typing import ClassVar
 
-from chift.api.mixins import ReadMixin
+from chift.api.mixins import ReadMixin, UpdateMixin
 from chift.openapi.models import SyncConsumer as SyncConsumerModel
 
 
-class Sync(ReadMixin[SyncConsumerModel]):
+class Sync(ReadMixin[SyncConsumerModel], UpdateMixin[SyncConsumerModel]):
     chift_vertical: ClassVar = "syncs"
     chift_model: ClassVar = ""
     model = SyncConsumerModel
