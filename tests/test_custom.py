493dfa8e755301977a5da960629d99761865a71c
Matthieu Hertoghe <matthieu.hertoghe@chift.eu>
2024-12-01 21:57:12 +0100

    adapt custom tests

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -35,33 +35,3 @@ def test_create_product(custom_regate_consumer: Consumer):
     product = consumer.custom.Custom.create("regate", "products", data)
 
     assert product
-
-
-def test_create_invoice(custom_regate_consumer: Consumer):
-    consumer = custom_regate_consumer
-
-    products = consumer.custom.Custom.all("regate", "products", limit=2)
-    product_id = products[0].get("id")
-
-    data = {
-        "currency": "EUR",
-        "payment_method": "bank_transfer",
-        "urgent": False,
-        "vat_paid": "receipts",
-        "customer_id": "16b9969d-5d89-4fc5-8a84-46d7dcd89f4d",
-        "delivery_date": datetime.datetime.now().strftime("%Y-%m-%d"),
-        "due_date": datetime.datetime.now().strftime("%Y-%m-%d"),
-        "lines": [
-            {
-                "quantity": 1,
-                "unit_price_cents": 100,
-                "sales_vat_account_id": "afe5673b-d379-4b6b-a2d5-187533497f78",
-                "product_id": product_id,
-            }
-        ],
-        "paid_amount_cents": 100,
-    }
-
-    invoice = consumer.custom.Custom.create("regate", "invoices", data)
-
-    assert invoice
